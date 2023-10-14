f2d1fd1be4eb536eac09c4412148ae40ce760481
shusuke-iruma-tea <shusuke.pana@gmail.com>
2023-10-14 19:46:12 +0900

    section68まで学習完了

--- README.md
+++ README.md
@@ -6,6 +6,9 @@
 ## サンプルサイト
 - https://github.com/budougumi0617/go_todo_app
 
+## .goldenの関連付け
+- https://kdnakt.hatenablog.com/entry/2022/08/13/associate-golden-files-to-json-in-vscode
+
 ##
 - go get -u "github.com/caarlos0/env/v6"
 - go get -u "golang.org/x/sync"
--- handler/add_task.go
+++ handler/add_task.go
@@ -1,1 +1,56 @@
 package handler
+
+import (
+	"encoding/json"
+	"net/http"
+	"time"
+
+	"github.com/go-playground/validator"
+	"github.com/iruma-tea/go_todo_app/entity"
+	"github.com/iruma-tea/go_todo_app/store"
+)
+
+type AddTask struct {
+	Store     *store.TaskStore
+	Validator *validator.Validate
+}
+
+func (at *AddTask) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	ctx := r.Context()
+	var b struct {
+		Title string `json:"title" validate:"required"`
+	}
+	if err := json.NewDecoder(r.Body).Decode(&b); err != nil {
+		ResponseJSON(ctx, w, &ErrResponse{
+			Message: err.Error(),
+		}, http.StatusInternalServerError)
+		return
+	}
+	err := at.Validator.Struct(b)
+	if err != nil {
+		ResponseJSON(ctx, w, &ErrResponse{
+			Message: err.Error(),
+		}, http.StatusBadRequest)
+		return
+	}
+
+	t := &entity.Task{
+		Title:   b.Title,
+		Status:  entity.TaskStatusTodo,
+		Created: time.Now(),
+	}
+
+	id, err := store.Tasks.Add(t)
+	if err != nil {
+		ResponseJSON(ctx, w, &ErrResponse{
+			Message: err.Error(),
+		}, http.StatusInternalServerError)
+		return
+
+	}
+	rsp := struct {
+		ID int `json:"id"`
+	}{ID: int(id)}
+
+	ResponseJSON(ctx, w, rsp, http.StatusOK)
+}
